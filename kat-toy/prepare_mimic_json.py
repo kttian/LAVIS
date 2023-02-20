e87b14bd8467c804bc79595cd3915f485ed13de8
kttian <ktian@college.harvard.edu>
2023-02-20 01:13:30 -0500

    modify script details to match lavis system

--- kat-toy/prepare_mimic_json.py
+++ kat-toy/prepare_mimic_json.py
@@ -25,10 +25,11 @@ import pandas as pd
 cache_root = "/n/data1/hms/dbmi/rajpurkar/lab/home/kt220/SPR23/export/home/.cache/lavis/"
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--report_mode', type=str, default='both', help='whether to generate findings, impression, or both')
+parser.add_argument('--report_mode', type=str, default='impression', help='whether to generate findings, impression, or both')
 parser.add_argument('--size_mode', type=str, default='subset', help='whether to generate mimic subset or full')
 parser.add_argument('--image', action='store_true') 
 parser.add_argument('--save', action='store_true') 
+parser.add_argument('--val', action='store_true') 
 
 
 data_train = pd.read_csv('mimic_train_reports.csv')
@@ -40,11 +41,13 @@ def convert_to_path(row):
     ''' 
     assumes input is a row of the dataframe
     ''' 
-    dicom_id = row["dicom_id"]
+    dicom_id = row['dicom_id']
     subject_id = str(int(row['subject_id']))
     study_id = str(int(row['study_id']))
-    img_file = f"p{subject_id[:2]}/p{subject_id}/s{study_id}/{dicom_id}.jpg"
-    return img_file 
+    img_file = f"p{subject_id[:2]}/p{subject_id}/s{study_id}/{dicom_id}.png"
+
+    image_id = "".join([str(int(x, 16)) for x in dicom_id.split('-')])
+    return img_file, dicom_id, f"p{subject_id}", f"s{study_id}", image_id 
 
 
 def get_report(row, report_mode):
@@ -59,6 +62,13 @@ def process_data(data_df):
     data_df.dropna()
 
 
+def get_filename(split, report_mode, size_mode):
+    outdir = os.path.join(cache_root, 'mimic_cxr', 'annotations')
+    name = f'mimic_{split}_{args.report_mode}_{args.size_mode}.json'
+    outfile = os.path.join(outdir, name)
+    return outdir, outfile 
+
+
 def main(args):
     subset_sizes = {'train': 64, 'val': 8, 'test': 8} # original sizes: ????
 
@@ -69,11 +79,9 @@ def main(args):
         size = subset_sizes[split]
         
         # create file path of where to save annotations json 
-        outdir = os.path.join(cache_root, 'mimic_cxr', 'annotations')
+        outdir, outfile = get_filename(split, args.report_mode, args.size_mode)
         if not os.path.exists(outdir):
             os.makedirs(outdir)
-        name = f'mimic_{split}_{args.report_mode}_{args.size_mode}.json'
-        outfile = os.path.join(outdir, name)
         
         # prepare data df for subset 
         # TODO: change data processing for full data later
@@ -83,9 +91,14 @@ def main(args):
         # loop through df and create json data 
         json_data = []
         for i, row in data_subset.iterrows():
-            json_item = {}
-            json_item['image'] = convert_to_path(row)
-            json_item['caption'] = [get_report(row, args.report_mode)]
+            img_file, dicom_id, subject_id, study_id, image_id = convert_to_path(row)
+            report = get_report(row, args.report_mode) 
+            if split == 'train':
+                report_item = report 
+            else:
+                report_item = [report]
+            json_item = {'image': img_file, 'caption': report_item, 'dicom_id': dicom_id, 
+                         'subject_id': subject_id, 'study_id': study_id, 'image_id': image_id}
             json_data.append(json_item)
         
         if args.save:
@@ -99,7 +112,11 @@ def main(args):
     if args.image:
         imgdir = os.path.join(cache_root, 'mimic_cxr', 'images')
         os.system(f'ln -s /n/data1/hms/dbmi/rajpurkar/lab/datasets/cxr/mimic-cxr-resized-256/2.0.0/files/ {imgdir}')
-
+    
+    if args.val:
+        _, test_path = get_filename('test', args.report_mode, args.size_mode)
+        _, val_path = get_filename('val', args.report_mode, args.size_mode)
+        os.system(f'cp {test_path} {val_path}')
 
 if __name__ == "__main__":
     args = parser.parse_args()
